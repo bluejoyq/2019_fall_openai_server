0989be0baaf1461e41f9592e4a613f50dcb7095a
bluejoyq <bluejoyq@naver.com>
2019-10-02 21:30:37 +0900

    separate need and not need

--- api/cliConnection.js
+++ api/cliConnection.js
@@ -18,10 +18,11 @@ const run=async()=>{
     let startTime = new Date().getTime();
     let x = await textAnalytic({"text":"감기 걸렸을 때 먹으면 좋은것은?"});
     let endTime = new Date().getTime();
+    //console.log(typeof(x));
+    console.log("need",x.need); 
+    console.log("notneed",x.notNeed); 
 
     console.log("걸리는 시간 :",endTime - startTime);
-    //console.log(typeof(x));
-    console.log(x); 
 };
 
 run();
--- api/testAnalystic.js
+++ api/testAnalystic.js
@@ -9,46 +9,102 @@ const argumentFrame = {
     "text":""
 };
 
-const getKeyword=(data)=>{
-    let pos = 0;
-    let posArray = [];
-    let temp=[]
-    let result = [];
-    for(let ch of data.text){
-        if(ch == ' '){
-            posArray.push(pos);
-        }
-        pos+=Buffer.byteLength(ch);
-    }
+const fineMorp = [ "NNG","NNP","NNB","VA","MM","SL","SH","SN","XPN","XSN","XSA","ETM"]
+const vvCheck = ["ETM","ETN","EC"];
 
-    let arrPos = 0;
-    data.morps.forEach(morp => {
-        if(posArray[arrPos]>morp.position){
-            temp.push(morp);
+
+const getMorp=(data)=>{
+    return new Promise((resolve,reject)=>{
+        let pos = 0;
+        let posArray = [];
+        let temp=[]
+        let result = [];
+        for(let ch of data.text){
+            if(ch == ' '){
+                posArray.push(pos);
+            }
+            pos+=Buffer.byteLength(ch);
         }
-        else{
-            console.log(posArray[arrPos]);
-            result.push(temp);
-            arrPos++;
-            temp=[];
-            temp.push(morp);
-        }
+
+        pos = 0;
+        data.morps.forEach(morp => {
+            if(pos==posArray.length){
+                temp.push(morp);
+                }
+            else if(posArray[pos]>morp.position){
+                temp.push(morp);
+                }
+            else{
+                result.push(temp); //원형 추가
+                pos++;
+                temp=[];
+                temp.push(morp);
+            }
+        });
+        result.push(temp); //원형 추가
+
+        resolve(result);
     });
-    return result;
 }
 
+const getKeyword=(data)=>{
+    let need = [];
+    let notNeed = [];
+
+    data.forEach((word)=>{
+        let needTemp = [];
+        let notNeedTemp = [];
+        let type = 0;
+        let check = false;
+        word.find(morp=>{
+            if(morp.type == "VV") type = 1;
+            if(morp.type == "XSV") type = 2;
+            if(morp.type == "VX") type = 3;
+        })
+        if(type == 3){
+            notNeed.push(word);
+        }
+        else if(type == 2){
+            need.push(word);
+        }
+        else if(type == 1){
+            for(let i = 0; i < word.length; i++){
+                if(vvCheck.indexOf(word[i].type)!=-1){
+                    check = true;
+                    need.push(word);
+                }
+            }
+            if(check == false){
+                notNeed.push(word);
+            }
+        }
+        else if(type == 0){
+            word.forEach((morp)=>{
+                if(fineMorp.indexOf(morp.type)!=-1){
+                    needTemp.push(morp);
+                }
+                else{
+                    notNeedTemp.push(morp);
+                }
+            })
+            if(notNeedTemp.length > 0) notNeed.push(notNeedTemp);
+            if(needTemp.length > 0) need.push(needTemp);
+        }
+    });
+
+    return({"need":need,"notNeed":notNeed});
+}
 
 const textAnalystic=(getData)=>{
     return new Promise(async(resolve,reject)=>{
         let argument = argumentFrame;
         argumentFrame.text = getData.text;
-
+        // 원래 텍스트 보존은?
         let result = await apiRequest(textMean,argument);
         let data ={}
         data.morps= result.return_object.sentence[0].morp;
         data.text = result.return_object.sentence[0].text
-        console.log(data);
-        resolve(getKeyword(data));
+        resolve(getMorp(data).then(getKeyword));
     });
     
 }
